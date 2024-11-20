7779364e44486ca678b9b8b4083a4c87d59b1d20
Ivy Yu <ivyyu7777777@gmail.com>
2024-11-20 21:12:51 +0000

    fixing control part

--- repo/rtl/Control/SignExtend.sv
+++ repo/rtl/Control/SignExtend.sv
@@ -4,13 +4,14 @@ module SignExtend (
     output      [31:0] imm_out
 );
 
-always_comb() begin
-    case (ImmSrc) begin
-        1'b0: imm_out = {{20{instr[31]}},instr[31:20]};             // I-type
-        1'b1: imm_out = {{20{instr[31]}},instr[31:25],instr[11:7]};  // S-type
+always_comb
+
+    case (ImmSrc)
+        2'b00: imm_out = {{20{instr[31]}}, instr[31:20]};             // I-type
+        2'b10: imm_out = {{20{instr[31]}}, instr[31], instr[7], instr[30:25],instr[11:8], 1'b0}; // B-type
         default: imm_out = 32'b0;
     endcase
-end 
 
 endmodule
+
 
