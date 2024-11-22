d25f924bac6e6ca50103865a08f6cfa3f6962039
RadaanMadhan <madhanradaan@gmail.com>
2024-11-22 12:34:09 +0100

    memory unit
    top memory and memory module added

---
+++ repo/rtl/top_memory.sv
@@ -0,0 +1,34 @@
+module top_memory #(
+    parameter  DATA_WIDTH = 32
+) (
+    input  logic                   clk, 
+    input  logic [DATA_WIDTH-1:0]  ALUResult,
+    input  logic [DATA_WIDTH-1:0]  WriteData,
+    input  logic                   ResultSrc,
+    input  logic                   MemWrite,
+    output logic [DATA_WIDTH-1:0]  Result
+);
+
+logic [DATA_WIDTH-1:0] ReadData;
+
+data_mem data_mem(
+    .clk        (clk),
+    .wr_en      (MemWrite),
+    .addr       (ALUResult),
+    .WriteData  (WriteData),
+    .ReadData   (ReadData)
+);
+
+
+mux ResultSlc(
+    .in0        (ALUResult),
+    .in1        (ReadData),
+    .sel        (ResultSrc),
+    .out        (Result)
+);
+
+
+
+
+
+endmodule
