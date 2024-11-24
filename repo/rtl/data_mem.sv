8de22ed5a479dd30e09f260ac0ca3c6eaa4079f4
RadaanMadhan <madhanradaan@gmail.com>
2024-11-24 21:31:41 +0100

    Data mem changed to little endian word addressing

--- repo/rtl/data_mem.sv
+++ repo/rtl/data_mem.sv
@@ -9,16 +9,29 @@ module data_mem #(
     output logic [DATA_WIDTH-1:0]          ReadData
 );
 
-logic [DATA_WIDTH-1:0] ram_array [2**ADDRESS_WIDTH-1:0];
+logic [7:0] ram_array [32'h0001FFFF:0];
 
+
+//---- Load sine coeff (Lab 4 extension) ----
+/*
+initial begin
+        $display("Loading Sine Coefficients");
+        $readmemh("../rtl/sinerom.mem", ram_array);  // Load into data mem
+end;
+*/
 
 
 always_ff @(posedge clk)
-    if (wr_en) ram_array[addr] <= WriteData;
+    if (wr_en) begin 
+        ram_array[addr + 3] <= WriteData [7:0];
+        ram_array[addr + 2] <= WriteData [15:8];
+        ram_array[addr + 1] <= WriteData [23:16];
+        ram_array[addr]     <= WriteData [31:24];
+    end
     
 
 always_comb
-    ReadData = ram_array[addr];
+    ReadData = {ram_array[addr + 3],ram_array[addr + 2],ram_array[addr+ 1],ram_array[addr]};
 
 
 endmodule
