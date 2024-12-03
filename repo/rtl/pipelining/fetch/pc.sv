86b62ea433a95afa65ba4007fb18be94ae15f21f
will03216 <zhangjunkaiwill@gmail.com>
2024-12-03 23:21:21 +0000

    pipeline fetch

---
+++ repo/rtl/pipelining/fetch/pc.sv
@@ -0,0 +1,18 @@
+module pc #(
+    parameter ADDRESS_WIDTH = 32
+)(
+    input wire clk,                       
+    input wire rst,                       
+    input wire [ADDRESS_WIDTH-1:0] pc_next,  
+    output reg [ADDRESS_WIDTH-1:0] pc     
+);
+
+    always_ff @(posedge clk or posedge rst) begin
+        if (rst) 
+            pc <= {ADDRESS_WIDTH{1'b0}};  
+        else 
+            pc <= pc_next;                
+    end
+
+endmodule
+
